1e24b59ee2846b3dac3871204531ad7a1d5af271
raushan <kumarraushan2615@gmail.com>
2025-03-21 22:49:43 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,65 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   
     <App />,
+  import { useState } from 'react'
+  import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+  import { atom } from 'recoil'
+  import './App.css'
+  import { counterAtom } from './assets/store'
   
+  function App() {
+  
+    return (
+      <RecoilRoot>
+        <Counter />
+      </RecoilRoot>
+      
+    )
+  }
+  
+  function Counter() {
+    return (
+      <div>
+        <CurrentCount />
+      <Increase /> 
+      <Decrease />
+      </div>
+      
+    )
+  }
+  
+  function CurrentCount() {
+    const count = useRecoilValue(counterAtom)
+    return (
+      <div>
+         {count}
+      </div>
+    )
+  }
+  
+  function Increase() {
+  
+    const setCount = useSetRecoilState(counterAtom)
+  
+    return (
+      <div>
+        <button onClick={() => {
+          setCount(c => c + 1)
+        }}>Increase</button>
+      </div>
+    )
+  }
+  
+  function  Decrease() {
+  
+    const setCount = useSetRecoilState(counterAtom)
+  
+    return (
+      <div>
+        <button onClick={() => {
+          setCount(c => c - 1)
+        }}>Decrease</button>
+      </div>
+    )
+  }
 )
